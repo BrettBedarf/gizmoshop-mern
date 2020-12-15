e1c28efec8c22f47c295005fae6acb51e56c0feb
BrettB <brettbedarf.dev@gmail.com>
2020-12-14 18:08:34 -0700

    fix app dev load

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,22 +1,19 @@
 import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 //connect to db and wait for verification succesfully
 const connectDb = async () => {
-	try {
-		const conn = await mongoose.connect(process.env.MONGO_URI, {
-			useNewUrlParser: true,
-			useUnifiedTopology: true,
-			useCreateIndex: true,
-		});
-		console.log(`MongoDB Connected: ${mongoose.connection.host}`);
-	} catch (e) {
-		`MongoDB connection error:\n${e}`;
-		//exit app with failure
-		process.exit(1);
-	}
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+    console.log(`MongoDB Connected: ${mongoose.connection.host}`);
+  } catch (e) {
+    console.log(`MongoDB connection error:\n${e}`);
+    //exit app with failure
+    process.exit(1);
+  }
 };
 
 export default connectDb;
--- backend/server.js
+++ backend/server.js
@@ -1,43 +1,28 @@
-<<<<<<< HEAD
-import express from 'express';
 import dotenv from 'dotenv';
 import products from './data/products.js';
 import connectDB from './config/db.js';
+import express from 'express';
+import mongoose from 'mongoose';
 
 //startup
 dotenv.config();
 connectDB();
-=======
-const express = require('express');
-const dotenv = require('dotenv');
-const products = require('./data/products.js');
-
-dotenv.config();
-
->>>>>>> 14a387ca7011928f261fba8a37fee4292ecbb90c
 const app = express();
 
 app.get('/', (req, res) => {
-	res.send('API Loading');
+  res.send('API Loading');
 });
 
 app.get('/api/products', (req, res) => {
-	res.json(products);
+  res.json(products);
 });
 
 app.get('/api/products/:id', (req, res) => {
-	const matchingProduct = find(p => p._id === req.params.id);
-	res.json(matchingProduct);
+  const matchingProduct = products.find((p) => p._id === req.params.id);
+  res.json(matchingProduct);
 });
 
 const port = process.env.PORT || 5000;
-app.listen(
-	port,
-	console.log(
-<<<<<<< HEAD
-		`Server running in ${process.env.NODE_ENV} mode on port ${port}`
-=======
-		`Server running in ${process.env.NODE_ENV} mode on ports ${port}`
->>>>>>> 14a387ca7011928f261fba8a37fee4292ecbb90c
-	)
+app.listen(port, () =>
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`)
 );
