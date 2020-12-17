6fbb81b9cca35a51cb7bd57e8a09af9e95cd7c9f
BrettB <brettbedarf.dev@gmail.com>
2020-12-17 15:48:13 -0700

    add redux store

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,37 +3,33 @@ import { Navbar, Nav, Container } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 
 const Header = () => {
-	return (
-		<header>
-			<Navbar bg='dark' variant='dark' collapseOnSelect expand='lg'>
-				<Container>
-					<LinkContainer to='/'>
-						<Navbar.Brand>GizmoShop</Navbar.Brand>
-					</LinkContainer>
-					<Navbar.Toggle aria-controls='basic-navbar-nav' />
-					<Navbar.Collapse id='basic-navbar-nav'>
-						<Nav className='ml-auto'>
-							<LinkContainer to='/login'>
-								<Nav.Link>
-									<i className='fas fa-user'></i> &nbsp;Sign
-									In
-								</Nav.Link>
-							</LinkContainer>
-							<LinkContainer to='/cart'>
-								<Nav.Link>
-									<i
-										className='fa fa-shopping-cart'
-										aria-hidden='true'
-									></i>{' '}
-									&nbsp;Cart
-								</Nav.Link>
-							</LinkContainer>
-						</Nav>
-					</Navbar.Collapse>
-				</Container>
-			</Navbar>
-		</header>
-	);
+  return (
+    <header>
+      <Navbar bg="dark" variant="dark" collapseOnSelect expand="lg">
+        <Container>
+          <LinkContainer to="/">
+            <Navbar.Brand>GizmoShop</Navbar.Brand>
+          </LinkContainer>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ml-auto">
+              <LinkContainer to="/login">
+                <Nav.Link>
+                  <i className="fas fa-user"></i> &nbsp;Sign In
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/cart">
+                <Nav.Link>
+                  <i className="fa fa-shopping-cart" aria-hidden="true"></i>{' '}
+                  &nbsp;Cart
+                </Nav.Link>
+              </LinkContainer>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
 };
 
 export default Header;
---
+++ frontend/src/store.js
@@ -0,0 +1,15 @@
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import Thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+const reducer = combineReducers({});
+const initialState = {}; //initial state when store loads
+const middleware = [Thunk];
+
+const store = createStore(
+  reducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
+
+export default store;
