f3df95b0ca084b8b8681f9da958057579f011a33
BrettB <brettbedarf.dev@gmail.com>
2020-12-24 18:03:10 -0700

    implement Loader and Message components on HomeScreen

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -2,6 +2,8 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Col, Row } from 'react-bootstrap';
 import ProductCard from '../components/ProductCard';
+import Loader from '../components/Loader';
+import Message from '../components/Message';
 import { listProducts } from '../actions/productActions';
 
 const HomeScreen = () => {
@@ -20,9 +22,9 @@ const HomeScreen = () => {
       {/* display loading while waiting for products to lead or error. 
           if successfull display latest products*/}
       {loading ? (
-        <h2>Loading...</h2>
+        <Loader />
       ) : error ? (
-        <h3>{error}</h3>
+        <Message variant="danger">{error}</Message>
       ) : (
         <Row>
           {products.map((product) => (
