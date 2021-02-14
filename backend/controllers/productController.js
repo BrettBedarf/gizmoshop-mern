84cdbb359785ac72fde7d8c4e0ec2696e3319efe
BrettB <brettbedarf.dev@gmail.com>
2021-02-14 04:13:04 -0700

    add product controller

---
+++ backend/controllers/productController.js
@@ -0,0 +1,33 @@
+import mongoose from 'mongoose';
+import asyncHandler from 'express-async-handler';
+import Product from '../models/productModel.js';
+
+// @ desc Fetch all products
+// @ route GET /api/products
+// @ access Public
+const getProducts = asyncHandler(async (req, res) => {
+  const products = await Product.find({}); //gets all products
+  res.json(products);
+});
+
+// @ desc Fetch a single product
+// @ route GET /api/products/:id
+// @ access Public
+const getProductById = asyncHandler(async (req, res) => {
+  const reqId = req.params.id;
+  if (mongoose.Types.ObjectId.isValid(reqId)) {
+    const product = await Product.findById({ _id: reqId });
+    //product found if it has an id
+    if (product.id) {
+      res.json(product);
+    } else {
+      res.status(404);
+      throw new Error('Product not found');
+    }
+  } else {
+    res.status(404);
+    throw new Error('Not a valid Product id');
+  }
+});
+
+export { getProducts, getProductById };
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,41 +1,13 @@
 import express from 'express';
 const router = express.Router();
-import mongoose from 'mongoose';
-import asyncHandler from 'express-async-handler';
-import Product from '../models/productModel.js';
+
+import {
+  getProducts,
+  getProductById,
+} from '../controllers/productController.js';
 
 export default router;
 
-// @ desc Fetch all products
-// @ route GET /api/products
-// @ access Public
-router.get(
-  '/',
-  asyncHandler(async (req, res) => {
-    const products = await Product.find({}); //gets all products
-    res.json(products);
-  })
-);
+router.route('/').get(getProducts);
 
-// @ desc Fetch a single product
-// @ route GET /api/products/:id
-// @ access Public
-router.get(
-  '/:id',
-  asyncHandler(async (req, res) => {
-    const reqId = req.params.id;
-    if (mongoose.Types.ObjectId.isValid(reqId)) {
-      const product = await Product.findById({ _id: reqId });
-      //product found if it has an id
-      if (product.id) {
-        res.json(product);
-      } else {
-        res.status(404);
-        throw new Error('Product not found');
-      }
-    } else {
-      res.status(404);
-      throw new Error('Not a valid Product id');
-    }
-  })
-);
+router.route('/:id').get(getProductById);
