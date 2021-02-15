a68b418df287ba83e9ad4fcba9d370f4a40b516b
BrettB <brettbedarf.dev@gmail.com>
2021-02-15 08:43:55 -0700

    reformat carousel

--- frontend/src/components/ProductCarousel.jsx
+++ frontend/src/components/ProductCarousel.jsx
@@ -21,25 +21,30 @@ const ProductCarousel = () => {
   ) : error ? (
     <Message variant="danger">{error}</Message>
   ) : (
-    <Carousel pause="hover" className="mb-3" interval="5000">
+    <Carousel
+      pause="hover"
+      className="mb-3"
+      interval="5000"
+      className="carousel-dark"
+    >
       {products.map((product) => (
-        <Carousel.Item key={product._id} className="bg-primary">
+        <Carousel.Item key={product._id} className="bg-white">
           <Link to={`/product/${product._id}`}>
             <Image
-              className="d-block w-100 m-auto"
+              className="d-block w-80 m-auto"
               style={{ padding: '40px' }}
               src={product.image}
               alt={product.name}
               fluid
             />
-            <Carousel.Caption
-              className="carousel-caption w-100 bg-primary"
+            {/* <Carousel.Caption
+              className="carousel-caption w-100 bg-white"
               style={{ right: '0', left: '0' }}
             >
               <h4>
                 {product.name} (${product.price})
               </h4>
-            </Carousel.Caption>
+            </Carousel.Caption> */}
           </Link>
         </Carousel.Item>
       ))}
