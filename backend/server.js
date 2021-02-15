983104a91bf932a35797cd53fb9ef0840e40c479
BrettB <brettbedarf.dev@gmail.com>
2021-02-15 05:42:32 -0700

    heroku Procfile and package.json scripts

--- backend/server.js
+++ backend/server.js
@@ -26,16 +26,20 @@ app.get('/api/config/paypal', (req, res) =>
 );
 
 const __dirname = path.resolve();
+
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '/frontend/build')));
 
-  // Any route that's not listed above, default to index.html in static folder
-  app.get('*', (req, res) =>
-    res.sendFile([path.resolve(__dirname, 'frontend', 'build', 'index.html')])
-  );
+  app.get('*', (req, res) => {
+    console.log(__dirname);
+    console.log(
+      res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    );
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  });
 } else {
   app.get('/', (req, res) => {
-    res.send('API is running in development mode..');
+    res.send('API is running....');
   });
 }
 
